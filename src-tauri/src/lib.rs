c0620484649df0ca06a9cf737ed7e41c46c8d65c
Ali Bassiouni <aabassiouni@hotmail.com>
2024-12-03 08:07:58 +0200

    trying things out

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,36 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tauri::Manager;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn get_windows() -> Vec<String> {
+    println!("getting windows");
+
+    let mut window_titles = Vec::new();
+
+    window_titles.push("window 1".to_string());
+    window_titles.push("window 2".to_string());
+
+    window_titles
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            let main_window = app.get_webview_window("main").unwrap();
+
+            main_window.center().expect("failed to center window");
+
+            main_window
+                .set_title("ali bassiouni")
+                .expect("failed to set title");
+            main_window
+                .set_closable(false)
+                .expect("failed to set closable");
+
+            Ok(())
+        })
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_windows])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
